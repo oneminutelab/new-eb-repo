449b6e00b62819511a8abc961e592eadf87e6e35
oneminutelab <93311011+oneminutelab@users.noreply.github.com>
2023-09-19 20:30:48 +0500

    files added

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require ("express");
+const cors = require ("cors");
+const app = express ();
+
+app.use (cors());
+app.get("/", (req, res) => res.json ("Hello! from iTechNets..."));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('server started on port ${PORT}'));
